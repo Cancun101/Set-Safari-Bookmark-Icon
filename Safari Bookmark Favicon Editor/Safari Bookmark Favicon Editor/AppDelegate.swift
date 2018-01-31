b90b15da2583c2d21773458f9b765a52dfdc46f0
Connor Gibson <connor@digitalmediaacademy.org>
2018-01-31 03:03:47 -0800

    Opened new Xcode Cocoa Application project

---
+++ Safari Bookmark Favicon Editor/Safari Bookmark Favicon Editor/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Safari Bookmark Favicon Editor
+//
+//  Created by Connor Gibson on 1/31/18.
+//  Copyright © 2018 Connor Gibson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
