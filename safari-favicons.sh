d4e9ebe27763194a0b4563a8de55cf5f33905741
Connor Gibson <connor@digitalmediaacademy.org>
2018-01-31 00:25:12 -0800

    Prettified script
    Expanded arguments to individual lines

--- safari-favicons.sh
+++ safari-favicons.sh
@@ -1,8 +1,12 @@
 #!/bin/zsh
 # Fix Safari Favorites Icons
 
-LSD="Library/Safari"; TIC="Touch Icons Cache"; TAB="cache_settings"; DIR="/$HOME/$LSD/$TIC/"
-DBF="$DIR/${${TIC// /}%s*}${${(C)TAB}/_/}.db"; SQL=$(sqlite3 "$DBF" "SELECT host FROM $TAB")
+LSD="Library/Safari"
+TIC="Touch Icons Cache"
+TAB="cache_settings"
+DIR="/$HOME/$LSD/$TIC/"
+DBF="$DIR/${${TIC// /}%s*}${${(C)TAB}/_/}.db"
+SQL=$(sqlite3 "$DBF" "SELECT host FROM $TAB")
 diff <(ls "$DIR/Images") <(for i in "${=SQL}"; do echo ${(U)$(md5 -qs $i)}.png; done | sort)
 sqlite3 "$DBF" "UPDATE $TAB SET ${${${(L)TIC// /_}#*_*}/s/_is_in}=1,download_status_flags=1"
 for file in "$1"/*.png; do URL="$(plutil -convert xml1 -o - "$HOME/$LSD/Bookmarks.plist" | \
